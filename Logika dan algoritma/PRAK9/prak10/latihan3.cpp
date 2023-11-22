04f7eee84c42377ad99d46e9e0b8839ceef3a755
cousin-rifki <rifkiibul@gmail.com>
2023-11-22 14:21:05 +0700

    Update terbaru

--- Logika dan algoritma/PRAK9/prak10/latihan3.cpp
+++ Logika dan algoritma/PRAK9/prak10/latihan3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 main(){
 
 int i,n;
-float nilai, jum=0, max =0,min=0;
+float nilai, jum=0, max =0,min=999999;
 
 cout<<"masukkan jumlah nilai :";
 cin>>n;
@@ -16,20 +16,19 @@ for (i=1 ;i<=n; i++)
     cout <<"Nilai ke :" <<i;
     cin>>nilai;
 
-    jum = jum+nilai;
+    jum = jum+nilai/n;
 
-if (i ==1){
-    max = nilai;
-    min = nilai;
+            if(max<nilai){
+                max = nilai;
+            }else{
+                max = max;
+            }
 
-}else{
-
-        if (max <nilai);
-            max =nilai;
-        if(min >nilai);
-            min = nilai;
-
-}
+            if(min>nilai){
+                min = nilai;
+            }else{
+                min = min;
+            }
 
 }
 
